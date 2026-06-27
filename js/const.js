5e47f873c0b6d44b14735137011b413a8b1dbc41
motherslovecafe <motherslovecafe@gmail.com>
2026-06-27 10:20:58 +0800

    Add files via upload

--- js/const.js
+++ js/const.js
@@ -1,4 +1,4 @@
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbzQEayD_4jUSLPS6f4DuvX2HMIiaBdHEPvnnWXgd8WK7SR3OIgMmqY1AAfnr2lrzI0T/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxBL4tOYYhqdDPeYtEY6MRwSAW3NJyPEleuaRov4-nfbYu09HZfenFXuqJTYsktmhQ-/exec';
 const YOUR_CLIENT_ID = '223887521833-37kdjp7rfucrpsidm1acciabl1soeegq.apps.googleusercontent.com';
 const YOUR_REDIRECT_URI = 'https://motherslovecafe.github.io';
 const lang_op = {

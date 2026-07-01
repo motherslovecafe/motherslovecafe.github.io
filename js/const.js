e8eb2e04e5acffd56e468e8d2a094f5dec0e894e
motherslovecafe <motherslovecafe@gmail.com>
2026-07-01 21:29:34 +0800

    Update const.js

--- js/const.js
+++ js/const.js
@@ -1,4 +1,4 @@
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbz8nctyaV_zSG9jLZfFYzrsO7Nt9rFOezjplM9QJqWD7L-vX0u-raa8341zz-1brPc/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbz3cnQqTrvfio60nHSfhnhLy6bAAeT9vvBu6_TNtL5w-uqUkTyOirAmlBfpiWORt537/exec';
 const YOUR_CLIENT_ID = '223887521833-37kdjp7rfucrpsidm1acciabl1soeegq.apps.googleusercontent.com';
 const YOUR_REDIRECT_URI = 'https://motherslovecafe.github.io';
 const lang_op = {

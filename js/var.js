172ec5e527605a6be9819d9241098917519ca643
motherslovecafe <motherslovecafe@gmail.com>
2026-07-17 13:28:33 +0800

    Update var.js

--- js/var.js
+++ js/var.js
@@ -1,12 +1,16 @@
 var labels = {};
 var lang = 'zh';
-var orderForm = {'ut':'','coffee_id':'cf001','coffee_pref':'H','byoc':false};
+var orderForm = {'ut':'','coffee_id':'cf001','coffee_pref':'H','extra':false,'byoc':false};
 var memForm = {'ut':'', 'item' : 'pi01', 'pt':50, 'desc':'愛心 Points', 'remarks' : false};
 var gid = '';
 var coffeeList = {};
 var prefHotOnlyList = [
   'cf002',
   'cf004'
+];
+var extraNAList = [
+  'cf009',
+  'cf010'
 ];
 var ptlist = {};
 var header = document.getElementById('container_header');
